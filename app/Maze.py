e81f694a9ea1ca60a7fbbf09b3f0c1b782cd815d
Thibault-MJS <thibault.minneboo.js@gmail.com>
2023-03-09 23:49:36 +0100

    Ajout base application + utilitaire test + méthodes de base

--- app/Maze.py
+++ app/Maze.py
@@ -1,3 +1,5 @@
+from random import choice, randint, shuffle
+
 class Maze:
     """
     Classe Labyrinthe
@@ -16,8 +18,8 @@ class Maze:
         """
         self.height    = height
         self.width     = width
-        self.neighbors = {(i,j): set() for i in range(height) for j in range (width)} if not empty else {(i,j): {(x, y) for x in range(i-1, i+2) for y in range(j-1, j+2) if (i, j) != (x, y) and 0 <= x <= height-1 and 0 <= y <= width-1} for i in range(height) for j in range(width)}
-
+        self.neighbors = {(i,j): set() for i in range(height) for j in range (width)} if not empty else {(i,j): {(y, x) for y in range(i-1, i+2) for x in range(j-1, j+2) if (i, j) != (y, x) and 0 <= y <= height-1 and 0 <= x <= width-1} for i in range(height) for j in range(width)}
+ 
     def info(self):
         """
         **NE PAS MODIFIER CETTE MÉTHODE**
@@ -40,6 +42,169 @@ class Maze:
             break
         txt += "- Structure cohérente\n" if valid else f"- Structure incohérente : {c1} X {c2}\n"
         return txt
+
+    def add_wall(self, c1: tuple, c2: tuple) -> None:
+        """
+        Ajoute un mur au labyrinthe entre la cellule c1 et la cellule c2.
+
+        Arguments:
+            - c1 (tuple): Un tuple contenant en première position la position y et en seconde position la position x de la cellule
+            - c2 (tuple): Un tuple contenant en première position la position y et en seconde position la position x de la cellule
+        """
+    # Facultatif : on teste si les sommets sont bien dans le labyrinthe
+        assert 0 <= c1[0] < self.height and \
+            0 <= c1[1] < self.width and \
+            0 <= c2[0] < self.height and \
+            0 <= c2[1] < self.width, \
+            f"Erreur lors de l'ajout d'un mur entre {c1} et {c2} : les coordonnées ne sont pas compatibles avec les dimensions du labyrinthe"
+        # Ajout du mur
+        if c2 in self.neighbors[c1]:      # Si c2 est dans les voisines de c1
+            self.neighbors[c1].remove(c2) # on le retire
+        if c1 in self.neighbors[c2]:      # Si c3 est dans les voisines de c2
+            self.neighbors[c2].remove(c1) # on le retire
+
+    def remove_wall(self, c1: tuple, c2: tuple) -> None:
+        """
+        Permet de supprimer un mur entre deux cellules.
+        """
+        assert 0 <= c1[0] < self.height and \
+            0 <= c1[1] < self.width and \
+            0 <= c2[0] < self.height and \
+            0 <= c2[1] < self.width, \
+            f"Erreur lors de la suppression d'un mur entre {c1} et {c2} : les cordonnées ne sont pas comptabibles avec les dimensions du labyrinthe"
+        
+        if c2 not in self.neighbors[c1]:
+            self.neighbors[c1].add(c2)
+        if c1 not in self.neighbors[c2]:
+            self.neighbors[c2].add(c1)
+
+    def get_walls(self) -> list:
+        """
+        Permet de récupérer tous les murs du tableau.
+        Le retour se présente sous la forme d'une liste contenant la liste de deux cellules.
+        """
+        walls = []
+
+        for c1 in self.neighbors.keys():
+            if (c1[0], c1[1]+1) in self.neighbors.keys() and (c1[0], c1[1]+1) not in self.neighbors[c1]:
+                walls.append([c1, (c1[0], c1[1]+1)])
+            if (c1[0]+1, c1[1]) in self.neighbors.keys() and (c1[0]+1, c1[1]) not in self.neighbors[c1]:
+                walls.append([c1, (c1[0]+1, c1[1])])
+
+        return walls 
+    
+    def fill(self) -> None:
+        """
+        Permet d'ajouter tous les murs possible au labyrinthe.
+        """
+        self.neighbors = {(i, j): set() for i in range(self.width) for j in range(self.height)}
+
+    def get_contiguous_cells(self, cell: tuple) -> list: 
+        """
+        Récupère l'entièreté des cellules contiguë à une cellule passée en paramètre.
+
+        Arguments:
+            - cell (tuple): Représente une cellule (un couple contenant la coordonnée x et la coordonnée y)
+        """
+        walls = []
+
+        if cell[0]-1 >= 0:
+            walls.append((cell[0]-1, cell[1]))
+        if cell[0]+1 < self.height:
+            walls.append((cell[0]+1, cell[1]))
+        if cell[1]-1 >= 0:
+            walls.append((cell[0], cell[1]-1))
+        if cell[1]+1 < self.width:
+            walls.append((cell[0], cell[1]+1))
+
+        return walls
+
+    @classmethod
+    def gen_btree(cls, height: int, width: int):
+        """
+        Génère un labyrinthe via l'algorithme de l'arbre binaire.
+
+        Arguments:
+            - height (int): représente la hauteur du labyrinthe
+            - width (int): représente la largeur du labyrinthe
+        """
+        laby = cls(height, width)
+
+        # On parcoure toutes les cellules du labyrinthe
+        for i in range(height):
+            for j in range(width):
+                # On tire au hasard une direction entre SOUTH et EAST
+                direction = choice(["SOUTH", "EAST"])
+
+                if direction == "SOUTH":
+                    # On regarde s'il y a un mur au sud de cette cellule
+                    if [(i,j), (i+1,j)] in laby.get_walls():
+                        # On supprime le mur au sud
+                        laby.remove_wall((i,j), (i+1,j))
+                    # Au contraire, on regarde s'il y a un mur à l'est de cette cellule
+                    elif [(i,j),(i,j+1)] in laby.get_walls():
+                        # On supprime le mur à l'est
+                        laby.remove_wall((i,j), (i,j+1))
+                else:
+                    # On regarde s'il y a un mur à l'est de cette cellule
+                    if [(i,j), (i,j+1)] in laby.get_walls():
+                        # On supprime le mur à l'est
+                        laby.remove_wall((i,j),(i,j+1))
+                    # Au contraire, on regarde s'il y a un mur au sud de cette cellule
+                    elif [(i,j), (i+1,j)] in laby.get_walls():
+                        # On supprime le mur au sud
+                        laby.remove_wall((i,j),(i+1,j))
+                
+        return laby
+
+    @classmethod
+    def gen_sidewinder(cls, height: int, width: int):
+        laby = cls(height, width)
+
+        for i in range(height-1):
+            # On initialise une variable sequence à une liste vide
+            sequence = []
+            for j in range(width-1):
+                # On ajoute la cellule à la sequence
+                sequence.append((i,j))
+
+                # On tire au hasard au pile ou face (PILE représente 0, FACE représente 1)
+                piece = randint(0,1)
+
+                # On vérifie si c'est face
+                if piece:
+                    # On choisie une cellule à casser parmi la séquence
+                    cell = choice(sequence)
+                    
+                    # On vérifie que le mur au sud est accessible
+                    if [cell, (cell[0]+1, cell[1])] in laby.get_walls():
+                        # On supprime le mur au sud de la cellule
+                        laby.remove_wall(cell, (cell[0]+1, cell[1]))
+
+                    # On réinitialise la sequence à vide
+                    sequence = []
+                else:
+                    # On vérifie que le mur à l'est est accessible
+                    if [(i,j), (i,j+1)] in laby.get_walls():
+                        # On supprime le mur à l'est de la cellule
+                        laby.remove_wall((i,j), (i,j+1))
+
+            # On ajoute le dernier mur à la séquence
+            sequence.append((i, width-1))
+
+            # On tire au sort une cellule parmi la séquence
+            cell = choice(sequence)
+
+            # On vérifie si son mur au sud est accessible
+            if [cell, (cell[0]+1, cell[1])] in laby.get_walls():
+                # On supprime le mur au sud de la cellule
+                laby.remove_wall(cell, (cell[0]+1, cell[1]))
+
+        # On casse tous les mur à l'est de la dernière ligne
+        for cell_x in range(width-1):
+            laby.remove_wall((height-1, cell_x), (height-1, cell_x+1))
+
+        return laby
 
     def __str__(self):
         """
--- tests.py
+++ tests.py
@@ -1,9 +1,100 @@
 from app.Maze import Maze
 
-print("-------------------------------")
-print("Test 1: Générer un labyrinthe totalement vide (avec aucun mur)")
-print("-------------------------------")
+print("""
+    _______       _______             __   
+    |   _   .--.--|       .-----.-----|  |_ 
+    |.  1   |  |  |.|   | |  -__|__ --|   _|
+    |.  ____|___  `-|.  |-|_____|_____|____|
+    |:  |   |_____| |:  |                   
+    |::.|           |::.|                   
+    `---'           `---'                   
+    """)
+print("Bienvenue sur l'interface de test")
+print("Afin d'avoir la liste des commandes disponibles veuillez taper '/help'")
 
-laby = Maze(4, 4, empty=True)
+print()
 
-print(laby)
+lst_commands = {
+    '/help': "Afficher la liste des commandes",
+    '/gen_btree <height> <width>': "Générer un labyrinthe par arbre binaire",
+    '/gen_sidewinder': "Générer un labyrinthe avec l'algorithme sidewinder",
+    '/gen_empty_maze <height> <width>': "Générer un labyrinthe vide",
+    '/gen_full_maze <height> <width>': "Générer un labyrinthe plein",
+    '/get_walls': "Récupère tous les murs du labyrinthe.",
+    '/gen_fusion_maze <height> <width>': "Générer un labyrinthe par fusion de chemin",
+    '/add_wall <cell1_posy> <cell1_posx> <cell2_posy> <cell2_posx>': "Ajoute un mur au labyrinthe"
+}
+
+cache = None
+
+while True:
+    try:
+        user_input = input('> ').split(" ")
+        cmd = user_input[0]
+        args = user_input[1:]
+
+        if cmd == '/help':
+            print("Voici la liste des commandes : ")
+            for command in lst_commands:
+                print(f"    {command} : {lst_commands[command]}")
+        elif cmd == '/gen_empty_maze':
+            if len(args) == 0:
+                print("Veuillez entrer la hauteur et la largeur du labyrinthe")
+            elif len(args) == 1:
+                print("Veuillez entrer la largeur du labyrinthe")
+            else:
+                laby = Maze(int(args[0]), int(args[1]), empty=True)
+                print(laby)
+                cache = laby
+        elif cmd == '/gen_full_maze':
+            if len(args) == 0:
+                print("Veuillez entrer la hauteur et la largeur du labyrinthe")
+            elif len(args) == 1:
+                print("Veuillez entrer la largeur du labyrinthe")
+            else:
+                laby = Maze(int(args[0]), int(args[1]))
+                print(laby)
+                cache = laby
+        elif cmd == '/gen_btree':
+            if len(args) == 0:
+                print("Veuillez entrer la hauteur et la largeur du labyrinthe")
+            elif len(args) == 1:
+                print("Veuillez entrer la largeur du labyrinthe")
+            else:
+                laby = Maze.gen_btree(int(args[0]), int(args[1]))
+                print(laby)
+                cache = laby
+        elif cmd == '/get_walls':
+            if cache is None:
+                print("Vous n'avez encore généré aucun labyrinthe. De ce fait, le labyrinthe n'est pas dans le cache")
+            else:
+                print("====== Murs du labyrinthe ======")
+                print(cache.get_walls())
+                print("================================")
+        elif cmd == '/add_wall':
+            if len(args) < 4:
+                print("Veuillez entrer la valeur y de la cellule 1, puis la valeur x de la cellule 1 et faire de même pour la cellule 2")
+            elif cache is None:
+                print("Vous n'avez encore généré aucun labyrinthe. De ce fait, le labyrinthe n'est pas dans le cache")
+            elif not (0 <= int(args[0]) < cache.height and 0 <= int(args[1]) < cache.width and 0 <= int(args[2]) < cache.height and 0 <= int(args[3]) < cache.width):
+                print(f"Les valeurs rentrées sont incorrectes, veuillez rentrer des valeurs possibles dans un labyrinthe {cache.height} x {cache.width}")
+            elif (int(args[2]), int(args[3])) not in cache.get_contiguous_cells((int(args[0]), int(args[1]))):
+                print("Veuillez entrer comme cellule 2 une cellule contiguë à la cellule 1")
+            else:
+                print("test")
+                cache.add_wall((int(args[0]), int(args[1])), (int(args[2]), int(args[3])))
+                print(f"Le mur ({args[0]}, {args[1]}), ({args[2]}, {args[3]}) a été ajouté avec succès")
+                print(laby)
+        elif cmd == '/gen_sidewinder':
+            if len(args) == 0:
+                print("Veuillez entrer la hauteur et la largeur du labyrinthe")
+            elif len(args) == 1:
+                print("Veuillez entrer la largeur du labyrinthe")
+            else:
+                laby = Maze.gen_sidewinder(int(args[0]), int(args[1]))
+                print(laby)
+                cache = laby
+        elif cmd == "exit":
+            exit(0)
+    except:
+        exit(0)
